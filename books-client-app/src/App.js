567e3bf2435ba170b48adf995bdf6e9f90ae3d3f
vivekduttamishra <vivek@conceptarchitect.in>
2022-01-17 16:01:19 +0530

    day#16 update#4

--- books-client-app/src/App.js
+++ books-client-app/src/App.js
@@ -3,14 +3,24 @@ import AppHeader from './components/AppHeader';
 import AppFooter from './components/AppFooter';
 import BookListScreen  from './screens/BookListScreen';
 import BookDetailsScreen from './screens/BookDetailsScreen';
-import BookListInfoScreen from './screens/BookListInfoScreen';
+import BookAddScreen from './screens/BookAddScreen';
+import UserLoginScreen from './screens/UserLoginScreen';
+import UserRegistrationScreen from './screens/UserRegistrationScreen';
+
+
 const App=({})=>{
+
 
     return <div className="App">
                <AppHeader title="World Wide Books" />
                <div className='Screen'>
-                    <BookListScreen visibility={false}  screenTitle="Books" />
-                    <BookDetailsScreen />
+
+                    <BookListScreen/>                    
+                    <BookDetailsScreen/>
+                    <BookAddScreen/>
+                    <UserLoginScreen/>
+                    <UserRegistrationScreen/>
+                   
                </div>
                <AppFooter />
             </div>
